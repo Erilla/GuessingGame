8ab7b53ff3f7f3221687c69fe4fb4a0e643e67ca
Ryan Wong <me@ryanwong.uk>
2021-06-06 16:34:40 +0100

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var targetNumber = Utilities.ChooseRandomInteger();
+            var gameWon = false;
+            Console.WriteLine(targetNumber);
+
+            int? previousGuess = null;
+
+            while (!gameWon)
+            {
+                try
+                {
+                    var guess = Utilities.RequestUserInput();
+
+                    if (targetNumber == guess)
+                    {
+                        Console.WriteLine("You got it!");
+                        gameWon = true;
+                    } else
+                    {
+                        var currentGuessDifference = Math.Abs(targetNumber - guess);
+
+                        if (previousGuess.HasValue)
+                        {
+                            if (previousGuess.Value == guess)
+                            {
+                                Console.WriteLine("Error: You have entered the same number!");
+                                continue;
+                            }
+
+                            var previousGuessDifference = Math.Abs(previousGuess.Value - targetNumber);
+
+                            if (currentGuessDifference < previousGuessDifference)
+                            {
+                                Console.WriteLine("Hotter");
+                            } else
+                            {
+                                Console.WriteLine("Colder");
+                            }
+                        } else
+                        {
+                            if (currentGuessDifference < 11)
+                            {
+                                Console.WriteLine("Hot");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Cold");
+                            }
+                        }
+
+                        previousGuess = guess;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("ERROR: Input was not a number.");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("ERROR: Input needs to be between 1 to 100.");
+                }
+            }
+
+            Utilities.RequestExitCommand();
+        }
+    }
+}
---
+++ GuessingGame/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    public static class Utilities
+    {
+        public static int ChooseRandomInteger()
+        {
+            Console.WriteLine("A random number has been chosen.");
+            return new Random().Next(1, 100);
+        }
+
+        public static int RequestUserInput()
+        {
+            Console.Write("Type your guess (1-100): ");
+            var input = int.Parse(Console.ReadLine());
+            if (input < 1 || input > 100) throw new ArgumentOutOfRangeException();
+            return input;
+        }
+
+        public static void RequestExitCommand()
+        {
+            Console.Write("Press to exit");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
